07ef4c88fde17df594f714dc57fe1edd610fe5d5
rosssabio2@gmail.com <rosssabio2@gmail.com>
2025-03-05 17:01:44 +0800

    changed the textField to passwordField

--- src/projectvantage/main/ProjectVantage.java
+++ src/projectvantage/main/ProjectVantage.java
@@ -11,6 +11,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -44,6 +45,7 @@ public class ProjectVantage extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.setTitle("Login");
         primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.getIcons().add(new Image("/projectvantage/resources/img/ProjectLogo.png"));
         primaryStage.show();
         config.setCenterAlignment(primaryStage);
     }
