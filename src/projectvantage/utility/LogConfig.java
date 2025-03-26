3dc0c35fd29b428d95701f211f0d796f62be8415
ScepticMaku <markjayvincentcortes@gmail.com>
2025-03-26 14:36:07 +0800

    implemented task 11 on login page

---
+++ src/projectvantage/utility/LogConfig.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectvantage.utility;
+
+/**
+ *
+ * @author SCC8
+ */
+public class LogConfig {
+    
+    dbConnect db = new dbConnect();
+    
+    private String action;
+    private String description;
+    
+    private void insertLog(int userId, String act, String desc) {
+        String sql = "INSERT INTO system_log (user_id, action, description) VALUES (?, ?, ?)";
+        
+        db.insertData(sql, userId, act, desc);
+    }
+    
+    public void logLogin(boolean isUserLoggedIn, int userId, String desc) {
+        
+        this.action = "Login Success";
+        this.description = desc;
+        
+        if(!isUserLoggedIn) {
+            this.action = "Login Failed";
+        }
+        
+        insertLog(userId, action, description);
+        
+        
+    }
+    
+}
